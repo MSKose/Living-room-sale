31a6061a2f291ad533d3541d63dbd4600046aebb
MSKose <saitmustafa1998@gmail.com>
2022-05-28 23:53:22 +0200

    onlick functionality added

---
+++ app.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    let blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    let popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
