be29326d6f6deb63547f682088f2e723a542c2fc
MSKose <saitmustafa1998@gmail.com>
2022-05-29 06:37:37 +0200

    popup screen added for the first item

--- app.js
+++ app.js
@@ -4,3 +4,19 @@ function myFunction() {
     let popup = document.getElementById('popup');
     popup.classList.toggle('active');
 }
+
+
+
+// function match(x) {
+//     const x = window.matchMedia("(max-width: 1400px)");
+//     const y = document.querySelectorAll('.col-4');
+
+//     if (x.matches && y.matches) {
+//         button.classList.replace('.col-4', '.col-6')
+//     }else {
+
+//     }
+// }
+
+
+  
